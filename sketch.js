5715b5d2a0edbf8d4cd95d2c418c894324e09d23
emydias7 <0000109035003xSP@al.educacao.sp.gov.br>
2024-05-29 13:01:19 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let cor 
+let x
+let y
+
+function setup() {
+
+  createCanvas(400, 400);
+  background(0,20,15);
+  cor=color(random(0,255),random(0,255),random(0,255));
+  x =200;
+  y=200
+}
+
+function draw() {
+  
+  fill(cor);
+  circle(x,y,50);
+  
+  if(mouseX < x){
+    x = x - 1
+  }
+  
+  if(mouseX > x){
+    x = x + 1
+  }
+  
+if(mouseY < y){
+  y --;
+}
+
+if(mouseY > y){
+  y++;
+}
+
+if(mouseIsPressed){
+  cor=color(random(0,255),random(0,255),random(0,255),random(0,100))
+}
+
+}
